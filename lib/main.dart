f7a2f74e2c71932f37281c0c380660d48323fc59
Dmytro <ignatov1988@gmail.com>
2023-08-29 13:43:49 +0300

    customized ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CryptoCurrenciesListApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CryptoCurrenciesListApp extends StatelessWidget {
+  const CryptoCurrenciesListApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CryptoCurrenciesList',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 29, 29, 29),
+        dividerColor: const Color.fromARGB(255, 49, 47, 47),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 29, 29, 29),
+          foregroundColor: Color.fromARGB(255, 29, 29, 29),
+          titleTextStyle: TextStyle(
+            color: Colors.amber,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+          centerTitle: true,
+        ),
+        textTheme: TextTheme(
+            bodyMedium: const TextStyle(
+              color: Colors.amber,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+            labelSmall: TextStyle(
+              color: Colors.white.withOpacity(0.6),
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            )),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text(
+          'Crypto Currencies List',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+          color: theme.dividerColor,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, i) => ListTile(
+          leading: SvgPicture.asset(
+            'assets/svg/bitcoin-sv-bsv-logo.svg',
+            height: 30,
+            width: 30,
+          ),
+          title: Text(
+            'Bitcoin',
+            style: theme.textTheme.bodyMedium,
+          ),
+          subtitle: Text(
+            '20000\$',
+            style: theme.textTheme.labelSmall,
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ),
+      ),
     );
   }
 }
